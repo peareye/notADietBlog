82e7ea69b64c7bba4b953842a447a8bd37a39f8c
Wolfgang Moritz <wolfmoritz@gmail.com>
2016-05-08 12:58:27 -0700

    Updated tables.sql definition.

--- tables.sql
+++ tables.sql
@@ -1,45 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 04, 2016 at 08:00 PM
--- Server version: 5.6.21
--- PHP Version: 5.5.19
-
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
--- --------------------------------------------------------
-
---
--- Table structure for table `post`
---
-
-CREATE TABLE IF NOT EXISTS `post` (
-`id` int(11) NOT NULL,
-  `title` varchar(60) NOT NULL,
-  `url` varchar(70) NOT NULL,
-  `url_locked` enum('N','Y') NOT NULL DEFAULT 'N',
-  `meta_description` varchar(160) DEFAULT NULL,
-  `content` text,
-  `content_html` text,
-  `content_excerpt` varchar(500) DEFAULT NULL,
-  `published_date` date DEFAULT NULL,
-  `created_by` int(11) NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL,
-  `updated_by` int(11) NOT NULL DEFAULT '1',
-  `updated_date` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comment`
---
-
 CREATE TABLE IF NOT EXISTS `comment` (
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `reply_id` int(11) NOT NULL DEFAULT '0',
   `post_id` int(11) NOT NULL,
   `name` varchar(60) NOT NULL,
@@ -49,13 +13,26 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `created_by` int(11) NOT NULL DEFAULT '1',
   `created_date` datetime NOT NULL,
   `updated_by` int(11) NOT NULL DEFAULT '1',
-  `updated_date` datetime NOT NULL,
-  KEY `post_approved_idx` (`approved`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
+  `updated_date` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
---
--- Table structure for table `session`
---
+CREATE TABLE IF NOT EXISTS `post` (
+`id` int(11) NOT NULL,
+  `title` varchar(60) NOT NULL,
+  `url` varchar(70) NOT NULL,
+  `url_locked` enum('N','Y') NOT NULL DEFAULT 'N',
+  `page` enum('N','Y') NOT NULL DEFAULT 'N',
+  `meta_description` varchar(160) DEFAULT NULL,
+  `content` text,
+  `content_html` text,
+  `content_excerpt` varchar(500) DEFAULT NULL,
+  `published_date` date DEFAULT NULL,
+  `template` varchar(50) DEFAULT NULL,
+  `created_by` int(11) NOT NULL DEFAULT '1',
+  `created_date` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL DEFAULT '1',
+  `updated_date` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1020 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `session` (
   `session_id` char(64) NOT NULL,
@@ -65,28 +42,18 @@ CREATE TABLE IF NOT EXISTS `session` (
   `time_updated` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `post`
---
+ALTER TABLE `comment`
+ ADD PRIMARY KEY (`id`), ADD KEY `post_approved_idx` (`approved`);
+
 ALTER TABLE `post`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `post_url_idx` (`url`), ADD KEY `post_published_idx` (`published_date`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `post_url_idx` (`url`), ADD KEY `post_published_idx` (`published_date`), ADD KEY `post_page_idx` (`page`);
 
---
--- Indexes for table `session`
---
 ALTER TABLE `session`
  ADD PRIMARY KEY (`session_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `post`
---
-ALTER TABLE `post`
+ALTER TABLE `comment`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+ALTER TABLE `post`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;SET FOREIGN_KEY_CHECKS=1;
